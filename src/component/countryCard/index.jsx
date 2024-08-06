57816302d4077c4b87a17a1b90fc6aad6a124b58
seoyejin <noonesyj@naver.com>
2024-08-07 03:16:45 +0900

    refactor: css 수정

--- src/component/countryCard/index.jsx
+++ src/component/countryCard/index.jsx
@@ -4,7 +4,7 @@ import "../../style/countryCard.css";
 export default function CountryCard({ country, toggleFavorite, isFavorite }) {
   return (
     <div className="countryCardContainer">
-      <div className="countryCardInfor">
+      <div className="countryCardInformation">
         <Link to={`/country/${country.cur_unit}`} className="countryItem">
           <h3>나라이름 x 통화 이름: {country.cur_nm}</h3>
           <p>통화코드: {country.cur_unit}</p>
